d35711fa17819620cea082dcb7fc0ef6b4479573
mogaming <seiya.orz@gmail.com>
2020-08-13 10:36:44 +0900

    improve

--- frontend/lib/firebase/client.ts
+++ frontend/lib/firebase/client.ts
@@ -1,12 +1,14 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
-;(() => {
+import 'firebase/analytics'
+
+const app = (() => {
   if (firebase.apps.length !== 0) {
     return firebase.app()
   }
 
-  return firebase.initializeApp({
+  const app = firebase.initializeApp({
     apiKey: process.env.PUBLIC_API_KEY,
     authDomain: process.env.PUBLIC_AUTH_DOMAIN,
     databaseURL: process.env.PUBLIC_DATABASE_URL,
@@ -14,9 +16,15 @@ import 'firebase/auth'
     storageBucket: process.env.PUBLIC_STORAGE_BUCKET,
     messagingSenderId: process.env.PUBLIC_MESSAGING_SENDER_ID,
     appId: process.env.PUBLIC_APP_ID,
-    measurementId: process.env.PUBLIC_MEASUREMENT_ID,
+    measurementId: process.env.PUBLIC_MEASUREMENT_ID
   })
+
+  return app
 })()
+
+if (process.browser) {
+  app.analytics()
+}
 
 export const firestore = firebase.firestore()
 export const auth = firebase.auth()
